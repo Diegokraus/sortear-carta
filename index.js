b9640c9aaa6cda916843dcfd3dde46b47e4bc408
Diegokraus <diegokraus@gmail.com>
2023-07-07 22:28:43 -0300

    atualizando

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-document.getElementById('tirarCarta').addEventListener('click',() => tirarUmaCartaAleatoriaDoBaralho())
+document
+  .getElementById("tirarCarta")
+  .addEventListener("click", () => tirarUmaCartaAleatoriaDoBaralho());
 
-async function criarBaralhoEmbaralhado(){
-    const url = 'https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+async function criarBaralhoEmbaralhado() {
+  const url =
+    "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
 
-    const resposta = await fetch(url)
+  const resposta = await fetch(url);
 
-    return await resposta.json()
-
+  return await resposta.json();
 }
 
-async function tirarUmaCarta(deck_id){
-    const url = `https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`
-    const resposta = await fetch(url)
-    return await resposta.json()
+async function tirarUmaCarta(deck_id) {
+  const url = `https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`;
+  const resposta = await fetch(url);
+  return await resposta.json();
 }
 
+async function tirarUmaCartaAleatoriaDoBaralho() {
+  const baralho = await criarBaralhoEmbaralhado();
+  const carta = await tirarUmaCarta(baralho.deck_id);
+  const imagemCarta = carta.cards[0].image;
 
-async function tirarUmaCartaAleatoriaDoBaralho(){
-    const baralho = await criarBaralhoEmbaralhado()
-    const carta = await tirarUmaCarta(baralho.deck_id)
-    const imagemCarta = carta.cards[0].image
-
-    document.getElementById('carta').src = imagemCarta
-
-
-
-
-
+  document.getElementById("carta").src = imagemCarta;
 }
 
-tirarUmaCartaAleatoriaDoBaralho()
+tirarUmaCartaAleatoriaDoBaralho();
